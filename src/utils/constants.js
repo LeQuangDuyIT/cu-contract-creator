5f9fd27f8d0fa341fdf58d5c712cae77935b859d
lqduycp <lqduycp@gmail.com>
2023-08-13 00:03:48 +0700

    install library & setup to simplify import

--- src/utils/constants.js
+++ src/utils/constants.js
@@ -1,90 +1,90 @@
 export const getId = () => new Date().getTime();
 
 export const datasParts = {
-    typePart: {
-        menuName: 'Contract type',
-        key: 'type'
-    },
-    clientPart: {
-        menuName: 'Client info',
-        key: 'client'
-    },
-    scopePart: {
-        menuName: 'Scope',
-        key: 'scope'
-    },
-    feePart: {
-        menuName: 'Service fee',
-        key: 'fee'
-    }
+  typePart: {
+    menuName: 'Contract type',
+    key: 'type'
+  },
+  clientPart: {
+    menuName: 'Client info',
+    key: 'client'
+  },
+  scopePart: {
+    menuName: 'Scope',
+    key: 'scope'
+  },
+  feePart: {
+    menuName: 'Service fee',
+    key: 'fee'
+  }
 };
 
 export const standardList = [
-    {
-        standardName: 'Organic',
-        standardValue: 'organic',
-        subStandards: [
-            {
-                subStandardName: 'EU 834/2007 & 889/2008',
-                subStandardId: 'EU-OLD'
-            },
-            {
-                subStandardName: 'New EU 848/2018',
-                subStandardId: 'EU-NEW'
-            },
-            {
-                subStandardName: 'USDA NOP',
-                subStandardId: 'NOP'
-            },
-            {
-                subStandardName: 'JAS',
-                subStandardId: 'JAS'
-            },
-            {
-                subStandardName: 'TOS',
-                subStandardId: 'TOS'
-            },
-            {
-                subStandardName: 'KOREA ORGANIC',
-                subStandardId: 'KOREA'
-            },
-            {
-                subStandardName: 'INPUT',
-                subStandardId: 'INPUT'
-            }
-        ]
-    },
-    {
-        standardName: 'Global GAP',
-        standardValue: 'globalgap',
-        subStandards: []
-    }
+  {
+    standardName: 'Organic',
+    standardValue: 'organic',
+    subStandards: [
+      {
+        subStandardName: 'EU 834/2007 & 889/2008',
+        subStandardId: 'EU-OLD'
+      },
+      {
+        subStandardName: 'New EU 848/2018',
+        subStandardId: 'EU-NEW'
+      },
+      {
+        subStandardName: 'USDA NOP',
+        subStandardId: 'NOP'
+      },
+      {
+        subStandardName: 'JAS',
+        subStandardId: 'JAS'
+      },
+      {
+        subStandardName: 'TOS',
+        subStandardId: 'TOS'
+      },
+      {
+        subStandardName: 'KOREA ORGANIC',
+        subStandardId: 'KOREA'
+      },
+      {
+        subStandardName: 'INPUT',
+        subStandardId: 'INPUT'
+      }
+    ]
+  },
+  {
+    standardName: 'Global GAP',
+    standardValue: 'globalgap',
+    subStandards: []
+  }
 ];
 
 export const initialDocDatas = {
-    [datasParts.typePart.key]: [],
-    [datasParts.clientPart.key]: [],
-    [datasParts.scopePart.key]: [],
-    [datasParts.feePart.key]: []
+  [datasParts.typePart.key]: [],
+  [datasParts.clientPart.key]: [],
+  [datasParts.scopePart.key]: [],
+  [datasParts.feePart.key]: []
 };
 
 export const initialTypeDatas = {
-    contractNum: '',
-    date: '',
-    aPRNum: '',
-    standard: standardList[0].standardValue,
-    subStandards: []
+  contractNum: '',
+  date: '',
+  aPRNum: '',
+  standard: standardList[0].standardValue,
+  subStandards: []
 };
 
 export const initialClientDatas = {
-    companyNameVi: '',
-    companyNameEn: '',
-    companyAddressVi: '',
-    companyAddressEn: '',
-    taxCode: '',
-    phoneNum: '',
-    representativeName: '',
-    pronouns: 'Mr.',
-    jobTitleVi: '',
-    jobTitleEn: ''
+  companyNameVi: '',
+  companyNameEn: '',
+  companyAddressVi: '',
+  companyAddressEn: '',
+  taxCode: '',
+  phoneNum: '',
+  representativeName: '',
+  pronouns: 'Mr.',
+  jobTitleVi: '',
+  jobTitleEn: ''
 };
